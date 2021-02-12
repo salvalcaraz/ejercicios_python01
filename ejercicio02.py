2b0333e75b45201b17c60338988a863faa89d0e6
salvalcaraz <s.alcarazg@gmail.com>
2021-02-12 10:51:29 +0100

    Add files via upload

---
+++ ejercicio02.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Script que pregunta el nombre del usuario en la consola y después de que el usuario lo introduzca muestre 
+ por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+
+"""
+
+name = input("Salva: ")
+print("¡Hola " + name + “!")
